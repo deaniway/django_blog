51a4004d05dd1c23c6c2a763009c361d5cb39739
Denis <ad.way@mail.ri>
2024-04-25 19:06:27 +0300

    step 4

--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('article')
+    app_name = 'DJANGO BLOG FOR DENISKA'  # Замените на реальное название вашего приложения
+    return render(request, 'articles/index.html', {'app_name': app_name})
